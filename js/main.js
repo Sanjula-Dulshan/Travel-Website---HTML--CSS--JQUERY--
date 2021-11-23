6fd00f2e9250c1a4a2c92de0210424fe2694c302
Sanjula-Dulshan <it20253530@my.sliit.lk>
2021-11-24 00:04:02 +0530

    feature section

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@ $(document).ready(function () {
     $(".menu .fa-bars").toggleClass("fa-times");
     $("section").toggleClass("nav-toggle");
   });
+
+  $(window).on("load scroll", function () {
+    $(".navbar").hide();
+    $(".menu .fa-bars").removeClass("fa-times");
+    $("section").removeClass("nav-toggle");
+  });
 });
